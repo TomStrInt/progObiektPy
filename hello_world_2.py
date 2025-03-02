f2ff549f224c1b8191f077e6328d55505b2677d3
student <student@iib.up.poznan.pl>
2025-03-02 16:52:34 +0100

    second commit

--- hello_world_2.py
+++ hello_world_2.py
@@ -1,8 +1,24 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtWidgets import QApplication, QPushButton, QLabel
+from PySide6.QtCore import Slot
+from PySide6.QtGui import QdoubleValidator
 
 app = QApplication(sys.argv)
 label = QLabel("Hello World!")
 label.show()
-app.exec()
 
+
+# Greetings
+@Slot()
+def say_hello():
+    print("Button clicked, Hello!")
+
+# Create a button
+
+button = QPushButton("Click me")
+# Connect the button to the function
+button.clicked.connect(say_hello)
+# Show the button
+button.show()
+# Run the main Qt loop
+app.exec()
